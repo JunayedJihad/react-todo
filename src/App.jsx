2fc390ce7a612a471055ebabb91ede69816c7a25
Junayed Jihad <junayedjihad48@gmail.com>
2023-10-27 06:44:21 +0600

    Reset Button Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,55 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-
-  const[list,setList] =React.useState([])
-  const [item, setItem] = React.useState("")
-  // const[input,setInput] = React.useState("")
+  const [list, setList] = React.useState([]);
+  const [item, setItem] = React.useState("");
+  const [value, setValue] = React.useState("");
+  const[isUpdated, setIsUpdated] = React.useState(false)
 
   React.useEffect(() => {
-    setList(JSON.parse(localStorage.getItem('data'))||[])
-
-  },[])
-
+    setList(JSON.parse(localStorage.getItem("data")) || []);
+  }, []);
 
   function handleChange(e) {
-    // setItem(input);
-    setItem(e.target.value)
-    // setInput(e.target.value)
-
+    setItem(e.target.value);
   }
 
   function add() {
-    if (item.length) {
-      list.push(item)
-      setList([...list]);
-      localStorage.setItem("data", JSON.stringify(list));
+    if (!isUpdated) {
+
+      if (item.length) {
+        list.push(item);
+        setList([...list]);
+        localStorage.setItem("data", JSON.stringify(list));
+      } else {
+        console.log("no text provided");
+      }
+
     } else {
-      console.log('no text provided')
+      if (item.length) {
+
+      } else {
+        console.log("no text provided");
+
+      }
     }
-    // setItem('')
-    // setInput('')
-    // console.log(input);
   }
 
-
   function remove(number) {
-    list.splice(number, 1)
-    setList([...list])
+    list.splice(number, 1);
+    setList([...list]);
     localStorage.setItem("data", JSON.stringify(list));
   }
 
   function edit(number) {
-
+    setIsUpdated(true);
   }
 
-
+  function reset() {
+    list.splice(0, list.length);
+    setList([...list]);
+    localStorage.clear()
+  }
 
   let listElements = list.map((listItem, index) => (
     <div key={index}>
@@ -55,9 +61,15 @@ const App = () => {
 
   return (
     <div>
-      <input type="text" onChange={handleChange} id="" placeholder='Add Something...'  />
-      <button onClick={add}>Add</button>
+      <input
+        type="text"
+        onChange={handleChange}
+        id=""
+        placeholder="Add Something..."
+      />
+      <button onClick={add}>{isUpdated?"Update":"Add"}</button>
       <div>{listElements}</div>
+      {list.length>0 && <button onClick={reset}>Delete All</button>}
     </div>
   );
 };
